8847e2417ba5c60409ef3a03d7926aaf951fdd9a
toqeer abbas <toqeernaqvi66@gmail.com>
2020-03-21 20:10:59 +0500

    Creating Migrations, Models, Factories

---
+++ database/factories/RepliesFactory.php
@@ -0,0 +1,12 @@
+<?php
+
+/** @var \Illuminate\Database\Eloquent\Factory $factory */
+
+use App\Model\replies;
+use Faker\Generator as Faker;
+
+$factory->define(replies::class, function (Faker $faker) {
+    return [
+        //
+    ];
+});
