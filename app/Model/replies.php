bf17cd6b9556d0936dec8bb7d51cdf36dec602ed
toqeer abbas <toqeernaqvi66@gmail.com>
2020-03-21 21:11:33 +0500

    Relationships

--- app/Model/replies.php
+++ app/Model/replies.php
@@ -3,8 +3,18 @@
 namespace App\Model;
 
 use Illuminate\Database\Eloquent\Model;
-
+use App\User;
 class replies extends Model
 {
-    //
+    public function question(){
+        return $this->belongsTo(Question::class);
+    }
+
+    public function like(){
+        return $this->hasMany(likes::class)
+    }
+
+    public function User(){
+        return $this->belongsTo(User::class);
+    }
 }
